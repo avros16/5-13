f749f4697c2b6334b3387d9364e28b6d2ee7f485
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-13 12:48:00 +0000

    Q1. Q2, Q3

--- .ccls-cache/@home@runner@513/main.cpp
+++ .ccls-cache/@home@runner@513/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+
+
+int values{0};
+
+  
+  cout << "How many values do you want the array to contain? ";
+  cin >> values;
+  
+int arr[values];
+  
+  for (int i =0; i<values; i++) {
+    cout << "input value number " << i+1 << ": ";
+    cin >> arr[i]; 
+    
+  }
+
+  cout << "array values: ";
+  
+  for (int i =0; i < values; i++) {
+    cout << arr[i] << " ";
+  }
+  ; 
+
+
+
+
+  
+  
+  
 }
